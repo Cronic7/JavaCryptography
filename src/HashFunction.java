050a6e91be2ff7aea67d4671d00fd6d5a9284759
cronic7 <cronic7@email.com>
2021-06-10 15:55:34 +0545

    1

---
+++ src/HashFunction.java
@@ -0,0 +1,83 @@
+import sun.security.provider.MD5;
+
+import java.security.MessageDigest;
+
+public class HashFunction {
+    //sha256 algorithm
+    static void sha256() throws Exception{
+        //data to be hashed or converted to message digest
+        String msg1="This is message 1";
+
+        //Creating the message digest objec
+        MessageDigest md1sha256= MessageDigest.getInstance("SHA-256"); //SHA-256 algorithm
+
+        //Passing data to the created messagedigest object
+        md1sha256.update(msg1.getBytes());
+
+        //Compute the message digest
+        byte[] digest= md1sha256.digest();
+        System.out.println(digest);
+
+        //Converting the byte array in to HexString format
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0;i<digest.length;i++) {
+            hexString.append(Integer.toHexString(0xFF & digest[i]));
+        }
+        System.out.println("Hex format : " + hexString.toString());
+
+    };
+    //md5 algorithm
+    static void md5()throws Exception{
+        //data to be hashed or converted to message digest
+        String msg1="This is message 1";
+
+        //Creating the message digest objec
+        MessageDigest md1md5= MessageDigest.getInstance("MD5"); //md-5 algorithm
+
+        //Passing data to the created messagedigest object
+        md1md5.update(msg1.getBytes());
+
+        //Compute the message digest
+        byte[] digest= md1md5.digest();
+        System.out.println(digest);
+
+        //Converting the byte array in to HexString format
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0;i<digest.length;i++) {
+            hexString.append(Integer.toHexString(0xFF & digest[i]));
+        }
+        System.out.println("Hex format : " + hexString.toString());
+
+    }
+    //sha1 algorithm
+    static void sha1() throws Exception{
+        //data to be hashed or converted to message digest
+        String msg1="This is message 1";
+
+        //Creating the message digest objec
+        MessageDigest md1sha1= MessageDigest.getInstance("SHA-1"); //SHA-1 algorithm
+
+        //Passing data to the created messagedigest object
+        md1sha1.update(msg1.getBytes());
+
+        //Compute the message digest
+        byte[] digest= md1sha1.digest();
+        System.out.println(digest);
+
+        //Converting the byte array in to HexString format
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0;i<digest.length;i++) {
+            hexString.append(Integer.toHexString(0xFF & digest[i]));
+        }
+        System.out.println("Hex format : " + hexString.toString());
+
+    };
+
+    public static void main(String  args[])throws Exception{
+        sha256();
+        md5();
+        sha1();
+
+
+    }
+}
